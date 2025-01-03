cf217606c9b5d5761236d1ffc11f9d3838be859d
mikku@arch <michael.lindemans@outlook.com>
2025-01-03 14:02:15 +0100

    finish enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(result) => self.move_position(result),
+            Message::Echo(result) => self.echo(result),
+            Message::ChangeColor(r, g, b) => self.change_color(r, g, b),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
