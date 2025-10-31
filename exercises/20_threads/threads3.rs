7f9a49e32eb2a8c16b728f22b28111b523fef721
mikkurogue <michael.lindemans@outlook.com>
2025-10-31 22:57:09 +0200

    do some shizzle with rustlings again i need to finish this one day...

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -1,4 +1,8 @@
-use std::{sync::mpsc, thread, time::Duration};
+use std::{
+    sync::{mpsc, Arc},
+    thread,
+    time::Duration,
+};
 
 struct Queue {
     first_half: Vec<u32>,
@@ -17,6 +21,9 @@ impl Queue {
 fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     // TODO: We want to send `tx` to both threads. But currently, it is moved
     // into the first thread. How could you solve this problem?
+    let tx = Arc::new(tx);
+    let tx_clone = Arc::clone(&tx);
+
     thread::spawn(move || {
         for val in q.first_half {
             println!("Sending {val:?}");
@@ -28,7 +35,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) {
     thread::spawn(move || {
         for val in q.second_half {
             println!("Sending {val:?}");
-            tx.send(val).unwrap();
+            tx_clone.send(val).unwrap();
             thread::sleep(Duration::from_millis(250));
         }
     });
